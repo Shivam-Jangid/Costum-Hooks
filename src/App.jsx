4e3b159d74a760bba351a9ed89107c71096c3a77
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-05-27 15:59:29 +0530

    debouncing hook is created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 // import usefetch from "./hooks/usefetch";
 // import usePrev from "./hooks/usePrev";
 import useDebounce from "./hooks/useDebounce";
-
 function App() {
-  const [data,setdata] = useState("");
+  const [inputVal,setinputVal] = useState("");
+  const debouncedVal = useDebounce(inputVal, 500);
+  useEffect(()=>{
+    console.log("heavy operation done ..."+ debouncedVal)
+
+  }, [debouncedVal])
   return <>
-  <input type="text" placeholder="Search here" onChange={(e)=>{setdata(e.target.value); useDebounce(data)}}/>
+  <input type="text" placeholder="Search here" onChange={(e)=>{setinputVal(e.target.value)}}/>
   </>
 }
 
--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,19 +1,28 @@
-let Clock;
-function sendDataToBackend(val){
-    console.log('data sent to backend...+ '+val);
+import { useEffect, useState } from "react";
+function useDebounce(val,delay) {
+    const [debouncedVal, setdebouncedVal] = useState(val);
+
+    useEffect(()=>{
+        const handler = setTimeout(() => {
+            setdebouncedVal(val)
+        }, delay);
+
+        return ()=>{clearTimeout(handler)}
+    }, [val, delay])
+
+    return debouncedVal;
 }
-function useDebounce(val){
-    clearTimeout(Clock);
-    Clock = setTimeout(() => {
-        sendDataToBackend(val)
-    },500);
+export default useDebounce;
 
-}
-export default useDebounce
+// function sendDataToBackend(val){
+//   //fetch request 
+//   console.log("val sent to backend .. "+ val)
+// }
 
 
-// use the below in app function in app.jsx
+// use the below code in app function
 //   const [data,setdata] = useState("");
+//   const debouncedFn = useDebounce(sendDataToBackend, data)
 //   return <>
-//   <input type="text" placeholder="Search here" onChange={(e)=>{setdata(e.target.value); useDebounce(data)}}/>
+//   <input type="text" placeholder="Search here" onChange={(e)=>{setdata(e.target.value);debouncedFn()}}/>
 //   </>
